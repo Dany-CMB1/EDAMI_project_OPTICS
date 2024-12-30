0237aa388431a6ade809ee1543ecc123552ddc00
Daniel B <daniel.bannister@proton.me>
2024-12-30 18:08:34 +0100

    Add: work on algorithms

---
+++ src/main/java/DObject.java
@@ -0,0 +1,89 @@
+
+import java.util.ArrayList;
+
+public abstract class DObject {
+    private int ID = 0;
+    private int clusterID = 0;
+    private double reachabilityDistance = 0;
+    private double coreDistance = Double.POSITIVE_INFINITY;
+    private boolean processed = false;
+    private ArrayList<Integer> neighbors = new ArrayList<>();
+
+    public abstract double distance(DObject p);
+    public abstract DObject extractData(String line);
+
+
+    //Getters
+    public int getID(){
+        return this.ID;
+    }
+
+    public boolean isProcessed(){
+        return this.processed;
+    }
+
+    public double getReachabilityDistance(){
+        return this.reachabilityDistance;
+    }
+
+    public double getCoreDistance(){
+        return this.coreDistance;
+    }
+
+    public int getClusterID(){
+        return this.clusterID;
+    }
+
+    public ArrayList<Integer> getNeighbors(){
+        return this.neighbors;
+    }
+
+    //Setters
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
+    public void setProcessed(){
+        this.processed = true;
+    }
+
+    public void setReachabilityDistance(double reachabilityDistance){
+        this.reachabilityDistance = reachabilityDistance;
+    }
+
+    public void setCoreDistance(ArrayList<DObject> D, int MinPts){
+
+        int countedNeighbors = 0;
+        for (int i=0; i< this.neighbors.size(); i++){
+            DObject n = D.get(this.neighbors.get(i));
+
+            if (this.distance(n) < this.coreDistance){
+                this.coreDistance = this.distance(n);
+            }
+            countedNeighbors++;
+        }
+
+        if (countedNeighbors < MinPts){
+            this.coreDistance = Double.POSITIVE_INFINITY;
+        }
+    }   
+
+    public void setClusterID(int clusterID){
+        this.clusterID = clusterID;
+    }
+
+    //Methods
+    public void findNeighbors(ArrayList<DObject> D, double epsilon){
+        for (int i=0; i< D.size(); i++){
+            DObject q = D.get(i);
+            if (this.getID() !=q.getID() && this.distance(q) <= epsilon){
+                this.neighbors.add(q.getID());
+            }
+        }
+    }
+
+}
+
+
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,48 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import static java.lang.Double.NaN;
+import java.util.ArrayList;
+
+import IrisPackage.Iris;
+import IrisPackage.IrisExtractionStrategy;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IrisExtractionStrategy strategy = new IrisExtractionStrategy();
+        strategy.extractData("data/iris/iris.data");
+
+        ArrayList<Iris> D = strategy.getData();
+   
+        for (int i=0; i< D.size(); i++){
+            D.get(i).findNeighbors(D, 3);
+        }
+    }
+
+    public static void OPTICS(ArrayList<DObject> D, double epsilon, int MinPts, FileOutputStream OrderedFile)  throws IOException { 
+
+        for (int i=0; i< D.size(); i++){
+            DObject q = D.get(i);
+            if (!q.isProcessed()){
+                ExpandClusterOrder(D, q, epsilon, MinPts, OrderedFile);
+            }
+        }
+        
+        OrderedFile.close();
+
+    }
+
+    public static void ExpandClusterOrder(ArrayList<DObject> D, DObject obj, double epsilon, int MinPts, FileOutputStream OrderedFile){
+        ArrayList<Integer> neighbors = obj.getNeighbors();
+        obj.setProcessed();
+        obj.setReachabilityDistance(NaN);
+
+        OrderedFile.write(obj.getID());
+
+
+    }
+
+}
